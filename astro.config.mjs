0890ba7a85cab6d640b1dbdcf762ea4eca6c2242
siebenbrunner <christoph@siebenbrunner.com>
2025-10-05 13:21:40 +0200

    Add URL base for github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://oxbridge-at.github.io/website/',
+  base: isProd ? "/website/" : "/",
   vite: {
     plugins: [tailwindcss()]
   }
