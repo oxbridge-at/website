b689b25660fa022d9864ef620704abb36998f6eb
siebenbrunner <christoph@siebenbrunner.com>
2026-03-27 22:44:49 +0100

    Change URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 const isProd = process.env.NODE_ENV === "production";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oxbridge-at.github.io/website/',
+  site: "https://www.oxbridge.at/",
   base: isProd ? "/website/" : "/",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
