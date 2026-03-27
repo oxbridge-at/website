2010144afe9d4060f4c1bcbf615e7b6382225705
siebenbrunner <christoph@siebenbrunner.com>
2026-03-27 22:46:31 +0100

    Remove /website (from Github Pages)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-const isProd = process.env.NODE_ENV === "production";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.oxbridge.at/",
-  base: isProd ? "/website/" : "/",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
